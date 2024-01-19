f8ffc4dcf61f4bd4e9252237376f4ea0755dd2b9
Ben <benarthurwil@icloud.com>
2024-01-19 13:10:56 -0500

    updated home.js for when logged in

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,11 @@ function Home() {
     const [selectedID, setSelectedID] = useState("");
     const [file, setFile] = useState("");
 
+    const [sentData, setSentData] = useState(`${Date.now()}: This is the data`);
+
+    const isLoggedIn = true;
+    const userID = 12345;
+
     const checkInputs = () => {
         if((file !== "" || text !== "") && selectedID !== ""){
             submitPressed();
@@ -13,6 +18,7 @@ function Home() {
             alert("Check to make sure the data field and the ID of the healthcare professional are both filled out.")
         }
     }
+
     
     const submitPressed = () => {
         console.log("ran");
@@ -22,31 +28,45 @@ function Home() {
         setFile("")
     }
 
-    return (
-        <>
-            <div className="Home">            
-                <div className="container mx-auto text-center">
-                    <form onSubmit={submitPressed}>
-                        <div className="input-group mb-3 pt-3">
-                            <span className="input-group-text" id="inputGroup-sizing-default">Type ID of Healthcare Professional:</span>
-                            <input value={selectedID} type="text" className="form-control" onChange={(event)=>setSelectedID(event.target.value)}/>
-                        </div>
-                        <div className="data-area">
-                            <textarea placeholder="Type or paste information here if applicable" className="form-control mb-5 mt-5" value={text} style={{borderColor: "black", height:"50vh", resize:"none"}} id="text" onChange={(event)=>setText(event.target.value)}></textarea>   
-                        </div>
-                        <div class="input-group mb-3">
-                            <label class="input-group-text" for="inputFile">Upload file here if applicable</label>
-                            <input onChange={(event)=>setFile(event.target.value)} value={file} type="file" class="form-control" id="inputFile"/>
-                        </div>
+    if(!isLoggedIn){
+        return (
+            <>
+                <div className="Home">            
+                    <div className="container mx-auto text-center">
+                        <form onSubmit={submitPressed}>
+                            <div className="input-group mb-3 pt-3">
+                                <span className="input-group-text" id="inputGroup-sizing-default">Type ID of Healthcare Professional:</span>
+                                <input value={selectedID} type="text" className="form-control" onChange={(event)=>setSelectedID(event.target.value)}/>
+                            </div>
+                            <div className="data-area">
+                                <textarea placeholder="Type or paste information here if applicable" className="form-control mb-5 mt-5" value={text} style={{borderColor: "black", height:"50vh", resize:"none"}} id="text" onChange={(event)=>setText(event.target.value)}></textarea>   
+                            </div>
+                            <div class="input-group mb-3">
+                                <label class="input-group-text" for="inputFile">Upload file here if applicable</label>
+                                <input onChange={(event)=>setFile(event.target.value)} value={file} type="file" class="form-control" id="inputFile"/>
+                            </div>
 
-                        <div className="submit-btn">
-                            <button className="btn btn-primary" type="button" onClick={checkInputs}>Submit form</button>
-                        </div>
-                    </form>
+                            <div className="submit-btn">
+                                <button className="btn btn-primary" type="button" onClick={checkInputs}>Submit form</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-            </div>
-        </>
-    );
+            </>
+        );
+    }
+    else if (isLoggedIn){
+        return(
+            <>
+                <div className="containter-fluid text-center p-3">
+                    <h1 className="fs-1 mt-3">Your ID: {userID}</h1>
+                    <div className="data-area">
+                        <textarea readOnly placeholder="Data will appear here when shared" className="form-control mb-5 mt-5" value={sentData} style={{borderColor: "black", height:"50vh", resize:"none"}} id="sent-data"></textarea>   
+                    </div>
+                </div>
+            </>
+        )
+    }
 }
 
 export default Home;
