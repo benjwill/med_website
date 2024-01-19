44072e064c453193808d61368f49049e2d06efaf
Ben <benarthurwil@icloud.com>
2024-01-19 12:07:41 -0500

    made NoPage

--- src/components/NoPage.js
+++ src/components/NoPage.js
@@ -1,5 +1,9 @@
-
 const NoPage = () => {
+    return (
+        <div className="container-fluid text-center mt-3">
+            <h1 className="fs-1">Error 404: Page not found</h1>
+        </div>        
+    )
 
 }
 
